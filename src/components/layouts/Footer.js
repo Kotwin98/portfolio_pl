c97b53ae00cc46ca4bec590b80b769c9a09ff9e4
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-22 14:17:19 +0200

    Improve mobile footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../../styles/layouts/Footer.scss";
 
 //fontawesome
@@ -8,24 +9,51 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return (
-        <p className="footer">
-            &copy;2020{" "}
-            <a href="https://github.com/Kotwin98/portfolio_v2">
+        <>
+            <ul className="footer">
+                <li>
+                <NavLink exact to="/about" className="footer_menu">
+                    About me
+                </NavLink>
+                </li>
+                <li>
+                <NavLink exact to="/skills" className="footer_menu">
+                    Skills
+                </NavLink>
+                </li>
+                <li>
+                <NavLink exact to="/portfolio" className="footer_menu">
+                    Portfolio
+                </NavLink>
+                </li>
+                <li>
+                <NavLink exact to="/contact" className="footer_menu">
+                    Contact
+                </NavLink>
+                </li>
+            </ul>
+            <p className="footer_links">
+                &copy;2020{" "}
+                <a href="https://github.com/Kotwin98/portfolio_v2">
                 <b>Mateusz Kotwiński</b>
-            </a>
-            <a className="footer_social" href="https://github.com/Kotwin98">
+                </a>
+                <a className="footer_social" href="https://github.com/Kotwin98">
                 <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a
+                </a>
+                <a
                 className="footer_social"
-                href="https://www.linkedin.com/in/mateusz-kotwi%C5%84ski/"
-            >
+                href="https://www.linkedin.com/in/mateusz-kotwi%C5%84ski"
+                >
                 <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a className="footer_social" href="mailto:mkotwinski98@gmail.com">
+                </a>
+                <a
+                className="footer_social footer_social--hidden"
+                href="mailto:mkotwinski98@gmail.com"
+                >
                 <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-        </p>
+                </a>
+            </p>
+        </>
     );
 };
 
