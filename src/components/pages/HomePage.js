5fff27919a1da8031a9ca16d7a9a8bc62512bae7
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-28 10:33:32 +0200

    Add home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "../../styles/pages/HomePage.scss";
 
 const HomePage = () => {
-  return <p>Welcome</p>;
+  return (
+    <section className="home">
+      <div className="home_welcome">
+        <span className="home_header">Hi!</span>
+        <p className="home_text">
+          My name is Mateusz Kotwiński.
+          About 18 months ago I planned to become a Front-end Developer.
+          I finished an intense 9 month long bootcamp with
+          help of an expirianced teacher - Bootcamp Web Developer Plus in Kodilla.
+          I also finished a few interesting courses on Udemy and I had spend a lot of time studying on my own.
+          Im still improving my skills, especially in JavaScript and React.
+          I have a lot of projects to show my knowledge.
+          Currently Im looking for my first job in IT. 
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
