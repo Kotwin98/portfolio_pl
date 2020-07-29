46b206ff0e3371ecaf2784d1f37ea905fc27a965
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-29 09:17:19 +0200

    Change visual

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "../../styles/pages/AboutPage.scss";
-//bootstrap
 import { Row, Col } from "react-bootstrap";
-//fontawesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const AboutPage = () => {
   return (
     <section className="about">
       <Row>
         <Col md={5}>
-          <span className="about_title">
-            <FontAwesomeIcon className="about_icon" icon={faCircle} /> Start
-          </span>
+          <span className="about_title">Start</span>
           <p className="about_text">
             My first steps with programming started when I acidently found a tutorial for c++ on youtube. It got me so intrested that immidiatly after the video I downloaded CodeBlocks and started writing my first lines of code.
             For the next few weeks I had been watching other parts of the tutorial and getting better with coding. I wanted to try something else that is more visiual and I got intrested with front-end.
           </p>
         </Col>
         <Col md={5}>
-          <span className="about_title">
-            <FontAwesomeIcon className="about_icon" icon={faCircle} /> Studying
-          </span>
+          <span className="about_title">Learning</span>
           <p className="about_text">
             From the start of 2019 I had been learning front-end. I finished an intense 9 month long bootcamp with help of an expirianced teacher - Bootcamp Kodilla Web Developer Plus.
             In the meantime I done few courses on Udemy such as React - The Complete Guide, The Complete JavaScript Course.
@@ -32,9 +24,7 @@ const AboutPage = () => {
       </Row>
       <Row>
         <Col md={10}>
-          <span className="about_title">
-            <FontAwesomeIcon className="about_icon" icon={faCircle} /> Currently
-          </span>
+          <span className="about_title">Currently</span>
           <p className="about_text">
             Nowadays I am looking for a job or an internship as Junior Front-end Developer. I learn very fast so I am also ready to take other positions with technologies diffrent than I know.
           </p>
