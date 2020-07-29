cbfeed6b71fcdf3bc75b9e53d261808af2d6e163
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-29 10:17:10 +0200

    Improve skills page

--- src/components/pages/SkillsPage.js
+++ src/components/pages/SkillsPage.js
@@ -24,77 +24,81 @@ import {
 const SkillsPage = () => {
   return (
     <section className="skills">
-      <div className="skills_header">
-        <p className="skills_title">Front-end</p>
+      <span className="skills_title">Front-end</span>
+      <div className="skills_stack">
+        <Row>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faHtml5} className="skills_icon" />
+            <p className="skills_lang">HTML</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faCss3Alt} className="skills_icon" />
+            <p className="skills_lang">CSS</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faSass} className="skills_icon" />
+            <p className="skills_lang">Sass</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faBootstrap} className="skills_icon" />
+            <p className="skills_lang">Bootstrap</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faJs} className="skills_icon" />
+            <p className="skills_lang">JavaScript</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faReact} className="skills_icon" />
+            <p className="skills_lang">React + Redux</p>
+          </Col>
+        </Row>
       </div>
-      <Row>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faHtml5} className="skills_icon" />
-          <p className="skills_lang">HTML</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faCss3Alt} className="skills_icon" />
-          <p className="skills_lang">CSS</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faSass} className="skills_icon" />
-          <p className="skills_lang">Sass</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faBootstrap} className="skills_icon" />
-          <p className="skills_lang">Bootstrap</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faJs} className="skills_icon" />
-          <p className="skills_lang">JavaScript</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faReact} className="skills_icon" />
-          <p className="skills_lang">React + Redux</p>
-        </Col>
-      </Row>
-      <p className="skills_title">Back-end</p>
-      <Row>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faNode} className="skills_icon" />
-          <p className="skills_lang">Node.js - Express</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
-          <p className="skills_lang">MongoDB</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faLayerGroup} className="skills_icon" />
-          <p className="skills_lang">Stack MERN</p>
-        </Col>
-      </Row>
-      <p className="skills_title">Tools and others</p>
-      <Row>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faGitAlt} className="skills_icon" />
-          <p className="skills_lang">Git</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faNpm} className="skills_icon" />
-          <p className="skills_lang">npm</p>
-        </Col>
-        <Col md={4}>
-          <FontAwesomeIcon icon={faYarn} className="skills_icon" />
-          <p className="skills_lang">Yarn</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <FontAwesomeIcon icon={faJira} className="skills_icon" />
-          <p className="skills_lang">Jira</p>
-        </Col>
-        <Col md={6}>
-          <FontAwesomeIcon icon={faSlack} className="skills_icon" />
-          <p className="skills_lang">Slack</p>
-        </Col>
-      </Row>
+      <span className="skills_title">Back-end</span>
+      <div className="skills_stack">
+        <Row>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faNode} className="skills_icon" />
+            <p className="skills_lang">Node.js - Express</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faEnvira} className="skills_icon" />
+            <p className="skills_lang">MongoDB</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faLayerGroup} className="skills_icon" />
+            <p className="skills_lang">Stack MERN</p>
+          </Col>
+        </Row>
+      </div>
+      <span className="skills_title">Tools and others</span>
+      <div className="skills_stack">
+        <Row>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faGitAlt} className="skills_icon" />
+            <p className="skills_lang">Git</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faNpm} className="skills_icon" />
+            <p className="skills_lang">npm</p>
+          </Col>
+          <Col md={4}>
+            <FontAwesomeIcon icon={faYarn} className="skills_icon" />
+            <p className="skills_lang">Yarn</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <FontAwesomeIcon icon={faJira} className="skills_icon" />
+            <p className="skills_lang">Jira</p>
+          </Col>
+          <Col md={6}>
+            <FontAwesomeIcon icon={faSlack} className="skills_icon" />
+            <p className="skills_lang">Slack</p>
+          </Col>
+        </Row>
+      </div>
     </section>
   );
 };
