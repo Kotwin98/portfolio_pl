2c8f531e82c3e2552697d28d508bb45254ceced6
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-07-29 11:22:48 +0200

    Add contact page

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -1,7 +1,81 @@
 import React from "react";
+import "../../styles/pages/ContactPage.scss";
+//bootstrap
+import { Row, Col } from "react-bootstrap";
+//fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons";
 
 const ContactPage = () => {
-  return <p>Contact</p>;
+  return (
+    <section className="contact">
+      <span className="contact_title">Contact</span>
+      <div className="contact_socials">
+        <Row>
+          <Col md={6}>
+            <a
+              className="contact_social"
+              target="_blank"
+              href="https://drive.google.com/open?id=1rf30FVVIhfXmYTAzHpbLHf8V_f_qtpAf"
+            >
+              <FontAwesomeIcon icon={faFilePdf} />
+            </a>
+            <a
+              target="_blank"
+              href="https://drive.google.com/open?id=1rf30FVVIhfXmYTAzHpbLHf8V_f_qtpAf"
+            >
+              Curriculum Vitae
+            </a>
+          </Col>
+          <Col md={6}>
+            <a
+              className="contact_social"
+              target="_blank"
+              href="mailto:mkotwinski98@gmail.com"
+            >
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+            <a href="mailto:mkotwinski98@gmail.com">
+              mailto:mkotwinski98@gmail.com
+            </a>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <a
+              className="contact_social"
+              target="_blank"
+              href="https://www.linkedin.com/in/mateusz-kotwi%C5%84ski/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/mateusz-kotwi%C5%84ski/"
+            >
+              linkedin.com/in/mateusz-kotwiński
+            </a>
+          </Col>
+          <Col md={6}>
+            <a
+              className="contact_social"
+              target="_blank"
+              href="https://github.com/Kotwin98"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/Kotwin98"
+            >
+              github.com/Kotwin98
+            </a>
+          </Col>
+        </Row>
+      </div>
+    </section>
+  );
 };
 
 export default ContactPage;
