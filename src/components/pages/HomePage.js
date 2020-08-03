064011ac96aab38b7e1265890945c5f5e3ab908f
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-08-03 12:05:13 +0200

    Improve home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,19 +4,17 @@ import "../../styles/pages/HomePage.scss";
 const HomePage = () => {
   return (
     <section className="home">
-      <div className="home_welcome">
-        <span className="home_header">Hi!</span>
-        <p className="home_text">
-          My name is Mateusz Kotwiński.
-          About 18 months ago I planned to become a Front-end Developer.
-          I finished an intense 9 month long bootcamp with
-          help of an expirianced teacher - Bootcamp Web Developer Plus in Kodilla.
-          I also finished a few interesting courses on Udemy and I had spend a lot of time studying on my own.
-          Im still improving my skills, especially in JavaScript and React.
-          I have a lot of projects to show my knowledge.
-          Currently Im looking for my first job in IT. 
-        </p>
-      </div>
+      <span className="home_header">Hi!</span>
+      <p className="home_text">
+        My name is Mateusz Kotwiński.
+        About 18 months ago I planned to become a Front-end Developer.
+        I finished an intense 9 month long bootcamp with
+        help of an expirianced teacher - Bootcamp Web Developer Plus in Kodilla.
+        I also finished a few interesting courses on Udemy and I had spend a lot of time studying on my own.
+        Im still improving my skills, especially in JavaScript and React.
+        I have a lot of projects to show my knowledge.
+        Currently Im looking for my first job in IT. 
+      </p>
     </section>
   );
 };
